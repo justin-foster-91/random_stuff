7303f26601d5e7f1158142b049935978915cd9f6
Justin Foster <jfoster91@gmail.com>
2020-08-26 17:28:30 -0400

    stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import AboutMenu from './about-menu';
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Router path='/about/:page' component={AboutMenu} />
         </div>
     );
 }
